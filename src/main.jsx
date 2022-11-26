78d6d08dd4419a25d9f878379148d673c3478aff
Mike Nieva <ae_manguevara@idcontractors.net>
2022-11-25 19:33:03 -0600

    React query integrado

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,15 @@ import {
   createBrowserRouter,
   RouterProvider
 } from 'react-router-dom'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools} from '@tanstack/react-query-devtools'
+
 import EjemploReactQuery from './pages/EjemploReactQuery'
 import Root from './pages/Root'
 import ErrorPage from './pages/ErrorPage'
+
+
 
 const router = createBrowserRouter([
   {
@@ -21,8 +27,13 @@ const router = createBrowserRouter([
   }
 ])
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}  />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}  />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
